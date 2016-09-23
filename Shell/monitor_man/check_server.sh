23f5585593d706bb8ef55cfd84e8f16a6e12d04f
Yc <1522105005@qq.com>
2016-09-23 18:26:53 +0800

    Yc

---
+++ Shell/monitor_man/check_server.sh
@@ -0,0 +1,9 @@
+###############################################
+# File Name:check_server.sh
+# Author: Yc
+# Mail : 1522105005@qq.com
+# Created Time: Sat 24 Sep 2016 09:38:56 AM CST
+#===============================================
+#!/bin/bash
+
+echo "check server"
---
+++ Shell/monitor_man/system_monitor.sh
@@ -0,0 +1,9 @@
+###############################################
+# File Name:system_monitor.sh
+# Author: Yc
+# Mail : 1522105005@qq.com
+# Created Time: Sat 24 Sep 2016 09:39:29 AM CST
+#===============================================
+#!/bin/bash
+
+echo "system system"
