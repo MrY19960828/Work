ca6c830bd6231b839a7d820d665205cebc20acaf
Yc <1522105005@qq.com>
2016-09-23 18:33:54 +0800

    Yc

--- monitor_man/check_http_log.sh
+++
@@ -1,9 +0,0 @@
-###############################################
-# File Name:check_http_log.sh
-# Author: Yc
-# Mail : 1522105005@qq.com
-# Created Time: Sat 24 Sep 2016 09:38:20 AM CST
-#===============================================
-#!/bin/bash
-
-echo "check http log"
--- monitor_man/system_monitor.sh
+++
@@ -1,9 +0,0 @@
-###############################################
-# File Name:system_monitor.sh
-# Author: Yc
-# Mail : 1522105005@qq.com
-# Created Time: Sat 24 Sep 2016 09:39:29 AM CST
-#===============================================
-#!/bin/bash
-
-echo "system system"
